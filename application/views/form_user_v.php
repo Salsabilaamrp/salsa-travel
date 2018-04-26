f5c67db9c21818279b9b9dfafa444350200e7967
salsabilaamrp <salsabilaamaraputri@gmail.com>
2018-04-26 14:56:12 +0700

    update

---
+++ application/views/form_user_v.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Input Data</title>
+</head>
+<body>
+	<form action="http://localhost/ci/index.php/user/add" method="POST">
+		<table>
+			<tr><td>Username</td><td> : </td><td><input name="username" type="text"></td></tr>
+			<tr><td>Password</td><td> : </td><td><input name="password" type="password"></td></tr>
+			<tr><td>Fullname</td><td> : </td><td><input name="fullname" type="text"></td></tr>	
+			<tr><td>Level</td><td>:</td><td><select name="level">
+				<option value="admin">Admin</option>
+				<option value="user">User</option></select></td></tr>
+			<tr><td><input name="tombol" type="submit" value="Simpan"></td></tr>
+		</table>
+	</form>
+</body>
+</html>
